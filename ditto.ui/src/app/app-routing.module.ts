fd3243eff554b20a382778805835213ff665d66a
PrabhjotxSingh <97115364+PrabhjotxSingh@users.noreply.github.com>
2023-02-25 13:38:25 -0500

    introduced angular

---
+++ ditto.ui/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{ path: '', component: LandingPageComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
