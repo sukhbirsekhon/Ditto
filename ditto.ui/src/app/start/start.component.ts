fe304fbe9ba847a00dba59e72547d22c13db4800
PrabhjotxSingh <97115364+PrabhjotxSingh@users.noreply.github.com>
2023-02-25 21:44:41 -0500

    button changes

--- ditto.ui/src/app/start/start.component.ts
+++ ditto.ui/src/app/start/start.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css']
+  styleUrls: ['./start.component.css'],
 })
 export class StartComponent {
+  speech: boolean = false;
 
+  ngOnInit() {}
+
+  startRecord() {
+    if (this.speech == true) {
+      this.speech = false;
+    } else {
+      this.speech = true;
+    }
+
+    if (this.speech == true) {
+    }
+  }
 }
